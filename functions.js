5c2058357ad974d458695dc1b22dee51520606c1
Aaron Williams <aaron@aaronwilliamsaudio.com>
2017-08-18 16:07:46 -0500

    Added more tiers
    Added calculations for more tiers

--- functions.js
+++ functions.js
@@ -11,12 +11,41 @@ function tierCalc(t2, t3) {
 		} else {
 			document.getElementById('answer').innerHTML = 0;
 		};
-	} else {
-		sum = Math.round((parseInt(t2)-parseInt(t3))*(3/4)/3);
+	} else if (optionVal == 't3-t4') {
+		sum = Math.round((parseInt(t2)-parseInt(t3))*(6/7)/6);
 		if (sum >= 0) {
 			document.getElementById('answer').innerHTML = sum;
 		} else {
 			document.getElementById('answer').innerHTML = 0;
 		};
+	} else if (optionVal == 't4-t5') {
+		sum = Math.round((parseInt(t2)-parseInt(t3))*(9/10)/9);
+		if (sum >= 0) {
+			document.getElementById('answer').innerHTML = sum;
+		} else {
+			document.getElementById('answer').innerHTML = 0;
+		};
+	} else if (optionVal == 't5-t6') {
+		sum = Math.round((parseInt(t2)-parseInt(t3))*(15/16)/15);
+		if (sum >= 0) {
+			document.getElementById('answer').innerHTML = sum;
+		} else {
+			document.getElementById('answer').innerHTML = 0;
+		};
+	} else if (optionVal == 't6-t7') {
+		sum = Math.round((parseInt(t2)-parseInt(t3))*(24/25)/24);
+		if (sum >= 0) {
+			document.getElementById('answer').innerHTML = sum;
+		} else {
+			document.getElementById('answer').innerHTML = 0;
+		};
+	} else if (optionVal == 't7-t8') {
+		alert('coming soon');
 	};
 };
+
+$("#high-tier").keyup(function(event){
+	if(event.keyCode == 13){
+		$("#submit").click();
+	}
+});
